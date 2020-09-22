f8f68c98bc97bd330202ff9918d31ebac4d3cc48
frewaeyaert2 <56259710+frewaeyaert2@users.noreply.github.com>
2020-09-22 11:46:14 +0200

    Add project files.

---
+++ DEVPROG_DEMO_02/MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+//make beer class accessible 
+using DEVPROG_DEMO_02.Model;
+
+//make Debug.WriteLine possible to write to output
+using System.Diagnostics;
+
+
+namespace DEVPROG_DEMO_02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CreateBeer();   //call the method on creation
+            LoadAllBeers();
+        }
+
+        /// <summary>
+        /// Creates a single beer instance and writes it to the output window.
+        /// </summary>
+        private void CreateBeer()
+        {
+            //-> do not forget the extra using statement to make Beer class accessible 
+            Beer omer = new Beer("Omer", "Bockor", 8, "blond");
+
+            //-> do not forget the extra using statement to make Debug accessible
+            //writes the entire beer object to the output window
+            //=> calls/shows the ToString function of the beer object
+            Debug.WriteLine(omer);
+
+            ShowDetails(omer);  //show details in GUI
+        }
+
+
+        /// <summary>
+        /// Shows the details of this beer object in the associated labels in the GUI (see: MainPage.xaml).
+        /// </summary>
+        /// <param name="beer">The beer object whose details you wish to show.</param>
+        private void ShowDetails(Beer beer)
+        {
+            //take the value of the name property,
+            //  and show it in the text part of the label lblName
+            lblName.Text = beer.Name;
+            lblBrewery.Text = beer.Brewery;
+            lblColor.Text = beer.Color;
+
+            //The text property only allows string objects, therefore
+            //  the alcohol percentage (type double) must be converted to a string
+            lblAlcohol.Text = beer.Alcohol.ToString();
+        }
+
+
+        /// <summary>
+        /// Uses the static GetBeers method of the Beer class to:
+        /// - get a hard coded list of beers
+        /// - print the beers in the list to the output window
+        /// </summary>
+        private void LoadAllBeers()
+        {
+            //call static method GetBeers of Beer class
+            List<Beer> allBeers = Beer.GetBeers();
+
+            //Show all beers in listview using ItemSource property
+            lstBeers.ItemsSource = allBeers;
+            //print all beers to output window using PrintBeers method
+            PrintBeers(allBeers);
+        }
+
+        /// <summary>
+        /// Prints all beers in a given list of beers one by one to the output window.
+        /// </summary>
+        /// <param name="beers">List of beers to be printed.</param>
+        private void PrintBeers(List<Beer> beerlist)
+        {
+            //loop through all Beer objects in beerlist
+            foreach (Beer beer in beerlist)
+            {
+                //create spaces for variables using {x}
+                //-> add the actual variables to the string format
+                //-> the order is associated with the index mentioned between {}
+                Debug.WriteLine("* {0} -> alcohol percentage: {1}", beer, beer.Alcohol);
+            }
+        }
+
+    }
+}
---
+++ DEVPROG_DEMO_02/Model/Beer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEVPROG_DEMO_02.Model
+{
+    //make class public
+    public class Beer
+    {
+        //1. PROPERTIES
+        public string Name { get; set; } //: Simple property "prop"
+
+        //Simple property alternative "propfull":
+        //private string _name;
+
+        //public string Name
+        //{
+        //    get { return _name; }
+        //    set { _name = value; }
+        //}
+
+        public string Brewery { get; private set; } //Readonly property
+
+        public double Alcohol { get; set; } //required type double
+
+        //Extra logic in set using "propfull"
+        private string _color;
+
+        public string Color
+        {
+            get { return _color; }
+            set
+            {
+                if (value != "")
+                {
+                    this._color = value;
+                }
+                else
+                {
+                    this._color = "unknown";
+                }
+            }
+        }
+
+
+        //2. CONSTRUCTORS
+
+        //each parameter has an explicit type
+        public Beer(string name, string brewery, double alcoholPerc, string color)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.Alcohol = alcoholPerc;
+            this.Brewery = brewery;
+        }
+
+        //3. METHODS
+
+        //default ToString (Python: def __str__(self): )
+        public override string ToString()
+        {
+            return Name + " (" + Brewery + ")";
+        }
+
+        /// <summary>
+        /// Get a hard coded list of beer objects
+        /// </summary>
+        /// <returns>the list of beer objects</returns>
+        public static List<Beer> GetBeers()
+        {
+            return new List<Beer>
+                {
+                    new Beer("Rodenbach Grand Cru", "Rodenbach", 6, "brown"),
+                    new Beer("Jupiler", "InBev", 5.2, "blond"),
+                    new Beer("Omer", "Bockor", 8, "blond"),
+                    new Beer("Duvel", "Duvel Moortgat", 8.5, "")
+                };
+        }
+
+
+        /// <summary>
+        /// Get the list of beers back filtered on alcohol percentage.
+        /// </summary>
+        /// <param name="beers">The full list of beers to be filtered.</param>
+        /// <param name="min">The minimum alcohol percentage of the beer.</param>
+        /// <param name="max">The maximum alcohol percentage of the beer.</param>
+        /// <returns>The filtered list of beers.</returns>
+        public static List<Beer> SearchByAlcohol(List<Beer> beers, double min, double max)
+        {
+            //create empty list to hold filtered beers 
+            List<Beer> results = new List<Beer>();
+
+            //loop through each beer in the beers list
+            //  (Python: for beer in beers: )
+            foreach (Beer beer in beers)
+            {
+                //check if alcohol percentage of the beer is between boundaries
+                if (beer.Alcohol >= min && beer.Alcohol <= max)
+                {
+                    //if so, add the beer to the list of results
+                    results.Add(beer);
+                }
+            }
+
+            //return the list of results
+            return results;
+        }
+
+        /// <summary>
+        /// Get the list of beers back filtered on (part of the) name.
+        /// </summary>
+        /// <param name="beers">Full list of beers to be filtered.</param>
+        /// <param name="searchTerm">(Part of) the name of the beer(s) to search for</param>
+        /// <returns></returns>
+        public static List<Beer> SearchByName(List<Beer> beers, string searchTerm)
+        {
+            //create empty list to hold filtered beers 
+            List<Beer> results = new List<Beer>();
+
+            //loop through each beer in the beers list
+            //  (Python: for beer in beers: )
+            foreach (Beer beer in beers)
+            {
+                //check if the name in lowercase contains the lowercase characters in searchterm 
+                if (beer.Name.ToLower().Contains(searchTerm.ToLower()))
+                {
+                    //if so, add the beer to the list of results
+                    results.Add(beer);
+                }
+            }
+
+            //return the list of results
+            return results;
+        }
+
+
+    }
+}
